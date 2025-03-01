bf6c62114645b6ee6526e1485ac984ead8c0937d
Le1NZ <pokrovskiypasha@yandex.ru>
2025-03-01 21:19:02 +0300

    Add ime insets for search screen and move SearchField to design-module

--- screens/search-screen/src/main/java/ru/pokrovskii/screen/search/ui/SearchScreenContent.kt
+++ screens/search-screen/src/main/java/ru/pokrovskii/screen/search/ui/SearchScreenContent.kt
@@ -1,36 +1,31 @@
 package ru.pokrovskii.screen.search.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import ru.pokrovskii.design.screen.ErrorScreen
+import ru.pokrovskii.design.screen.LoadingScreen
+import ru.pokrovskii.design.search.SearchField
 import ru.pokrovskii.design.theme.AppTheme
 import ru.pokrovskii.design.toolbar.AppToolbar
 import ru.pokrovskii.design.toolbar.ToolbarConfig
 import ru.pokrovskii.design.toolbar.ToolbarIcon
 import ru.pokrovskii.screen.search.R
-import ru.pokrovskii.design.screen.ErrorScreen
-import ru.pokrovskii.design.screen.LoadingScreen
 import ru.pokrovskii.screen.search.ui.state.SearchScreenState
 import ru.pokrovskii.screen.search.ui.success.SearchScreenSuccess
 import ru.pokrovskii.screen.search.viewmodel.SearchScreenPresenter
 import ru.pokrovskii.screen.search.viewmodel.SearchScreenPresenterPreview
-import ru.pokrovskii.design.R.drawable
 
 @Composable
 internal fun SearchScreenContent(
@@ -52,39 +47,29 @@ internal fun SearchScreenContent(
             )
         )
 
-        TextField(
-            modifier = Modifier
-                .padding(vertical = 8.dp)
-                .padding(horizontal = 16.dp)
-                .fillMaxWidth(),
-            value = query,
-            onValueChange = presenter::onQueryChanged,
-            placeholder = { Text(text = stringResource(R.string.enter_query)) },
-            trailingIcon = {
-                if (query.isNotEmpty()) {
-                    IconButton(
-                        onClick = { presenter.onQueryChanged("") },
-                    ) {
-                        Icon(
-                            painter = painterResource(id = drawable.ic_close_24),
-                            contentDescription = null,
-                        )
-                    }
-                }
-            },
-            singleLine = true,
-            maxLines = 1,
+        SearchField(
+            query = query,
+            onQueryChanged = presenter::onQueryChanged,
+            placeholderText = stringResource(R.string.enter_query),
         )
 
-        when (state) {
-            is SearchScreenState.Loading -> LoadingScreen()
-            is SearchScreenState.Error -> ErrorScreen(
-                onRetryClick = presenter::onRetryClick,
-            )
-            is SearchScreenState.Success -> SearchScreenSuccess(
-                state = state,
-                presenter = presenter,
-            )
+        Box(
+            modifier = Modifier
+                .imePadding()
+                .fillMaxSize(),
+        ) {
+            when (state) {
+                is SearchScreenState.Loading -> LoadingScreen()
+
+                is SearchScreenState.Error -> ErrorScreen(
+                    onRetryClick = presenter::onRetryClick,
+                )
+
+                is SearchScreenState.Success -> SearchScreenSuccess(
+                    state = state,
+                    presenter = presenter,
+                )
+            }
         }
     }
 }
